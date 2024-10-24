40bff906cee6fd0fd400fc4025fe173780a308b9
santhosh <santhoshprince6@gmail.com>
2024-10-24 16:28:31 +0530

    fixed

--- src/componets/card.js
+++ src/componets/card.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "../componets/card.css";
 
-const card = ({ value, isFlipped, onClick }) => {
+const Card = ({ value, isFlipped, onClick }) => {
   return (
     <div className="card" onClick={onClick}>
       <div className={isFlipped ? "fliped" : ""}>
-      
-      <div className="front">
-        {isFlipped && (
-            <span>{value}</span>
-        )}
-
-      </div>
-
-        <div className="back" onClick={onClick}>
-          ?
+        <div className="front">
+          {isFlipped && <span>{value}</span>}
         </div>
+        <div className="back">?</div>
       </div>
     </div>
   );
 };
-export default card;
+
+export default Card;
